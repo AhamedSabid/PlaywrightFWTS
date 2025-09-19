05da732af9ab2be2e10ec966c68b5e9ffd62d685
Ahamed <ahamed.nasar@insurity.com>
2025-09-19 12:24:47 +0530

    Created random utility function

--- src/tests/Register.spec.ts
+++ src/tests/Register.spec.ts
@@ -1,7 +1,11 @@
 import { test, expect } from '@playwright/test';
 import { RegisterPage } from '../pages/RegisterPage';
 import registerData from '../data/registerData.json';
+import { getRandomString } from '../util/utilities';
 
+//call getRandomString and update the json data
+const randomStr = getRandomString(5);
+registerData.firstName = `AutoFirst${randomStr}`;
 
 test('Register test', async ({ page }) => {
   const registerPage = new RegisterPage(page);
---
+++ src/util/utilities.ts
@@ -0,0 +1,10 @@
+// Utility functions for generating random data and formatting strings
+export function getRandomString(length: number): string {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+  return result;
+}
